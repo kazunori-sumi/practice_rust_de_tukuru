d44489a67463f21b935a0d3009990e220c1214b7
kazunori-sumi <ksumi1988@icloud.com>
2024-11-06 23:46:55 +0900

    除算機能追加

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
                         "+" => add(&mut stack),
                         "-" => sub(&mut stack),
                         "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
                         _ => panic!("{word:?} could not be parsed"),
                     }
                 }
@@ -41,4 +42,11 @@ fn mul(stack: &mut Vec<i32>) {
     let rhs = stack.pop().unwrap();
 
     stack.push(lhs * rhs);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+
+    stack.push(lhs / rhs);
 }
