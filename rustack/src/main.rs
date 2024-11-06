c3cb69877f233efe359575c960526fd70f2e636a
kazunori-sumi <ksumi1988@icloud.com>
2024-11-06 23:41:49 +0900

    乗算機能追加

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
                     match word {
                         "+" => add(&mut stack),
                         "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
                         _ => panic!("{word:?} could not be parsed"),
                     }
                 }
@@ -33,4 +34,11 @@ fn sub(stack: &mut Vec<i32>) {
     let rhs = stack.pop().unwrap();
 
     stack.push(lhs - rhs);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+
+    stack.push(lhs * rhs);
 }
